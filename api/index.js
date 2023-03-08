15d32698e70d647233a06c4f858c28e4907ed83a
Christopher Kade <christopher.kade@openclassrooms.com>
2023-03-08 10:27:45 +0100

    Using openai package

--- api/index.js
+++ api/index.js
@@ -1,17 +1,50 @@
 const app = require("express")();
-const { v4 } = require("uuid");
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
 
-app.get("/api", (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
-app.get("/api/item/:slug", (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
+const openai = new OpenAIApi(configuration);
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    console.log("PROMPT", prompt);
+
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+    });
+
+    console.log("RESPONSE", response.data.choices[0].message.content);
+
+    res.status(200).send({
+      bot: response.data.choices[0].message.content,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error || "Something went wrong");
+  }
+
+  // res.setHeader("Content-Type", "text/html");
+  // res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  // res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
+
+// app.get("/api/item/:slug", (req, res) => {
+//   const { slug } = req.params;
+//   res.end(`Item: ${slug}`);
+// });
 
 module.exports = app;
 
@@ -54,9 +87,5 @@ module.exports = app;
 //     res.status(500).send(error || "Something went wrong");
 //   }
 // });
-
-// app.listen(5001, () =>
-//   console.log("AI server started on http://localhost:5001")
-// );
 
 // module.exports = app;
